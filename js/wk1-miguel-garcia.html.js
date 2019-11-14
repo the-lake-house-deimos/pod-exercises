eb1693d52b813a4256c67dacd51c52c2fe867fc5
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-11-14 10:56:10 -0600

    Completed exercise except. Last test has floating point precision problem

--- js/wk1-miguel-garcia.html.js
+++ js/wk1-miguel-garcia.html.js
@@ -1,5 +1,40 @@
 // ( function () {
    "use strict";
 
+let test1 = [
+   { product: "Milk", quantity: 1, price: 1.50 }
+];
+let test2 = [
+   { product: "Milk", quantity: 1, price: 1.50 },
+   { product: "Cereals", quantity: 1, price: 2.50 }
+];
+let test3 = [
+   { product: "Milk", quantity: 3, price: 1.50 }
+];
+let test4 = [
+   { product: "Milk", quantity: 1, price: 1.50 },
+   { product: "Eggs", quantity: 12, price: 0.10 },
+   { product: "Bread", quantity: 2, price: 1.60 },
+   { product: "Cheese", quantity: 1, price: 4.50 }
+];
+let test5 = [
+   { product: "Chocolate", quantity: 1, price: 0.10 },
+   { product: "Lollipop", quantity: 1, price: 0.20 }
+];
+
+const getTotalPrice = groceries => {
+  let amount = 0;
+  groceries.forEach((produce) => {
+     amount += (produce.quantity) * (produce.price);
+  });
+   return amount;
+};
+
+
+console.log(getTotalPrice(test1));
+console.log(getTotalPrice(test2));
+console.log(getTotalPrice(test3));
+console.log(getTotalPrice(test4));
+console.log(getTotalPrice(test5));
 
 // })();
