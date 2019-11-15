8f720d11cec80e6a2b4b9dc80046667342df302f
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-11-15 09:25:37 -0600

    Completed jackpot check pod

--- js/wk1-miguel-garcia.js
+++ js/wk1-miguel-garcia.js
@@ -41,4 +41,34 @@ console.log(getTotalPrice(test3));
 console.log(getTotalPrice(test4));
 console.log(getTotalPrice(test5));
 
+console.log("-----------");
+
+let jackpotTest1 = ['@', '@', '@', '@'];
+let jackpotTest2 = ['!', '!', '!', '!'];
+let jackpotTest3 = ['abc', 'abc', 'abc', 'abc'];
+let jackpotTest4 = ['karaoke', 'karaoke', 'karaoke', 'karaoke'];
+let jackpotTest5 = ['SS', 'SS', 'SS', 'SS'];
+let jackpotTest6 = [':(', ':)', ':|', ':|'];
+let jackpotTest7 = ['&&', '&', '&&&', '&&&&'];
+let jackpotTest8 = ['hee', 'heh', 'heh', 'heh'];
+let jackpotTest9 = ['SS', 'SS', 'SS', 'Ss'];
+let jackpotTest10 = ['SS', 'SS', 'Ss', 'Ss'];
+
+const testJackpot = result => {
+  let mySet = new Set(result);
+    return mySet.size === 1;
+};
+
+console.log(testJackpot(jackpotTest1));
+console.log(testJackpot(jackpotTest2));
+console.log(testJackpot(jackpotTest3));
+console.log(testJackpot(jackpotTest4));
+console.log(testJackpot(jackpotTest5));
+console.log(testJackpot(jackpotTest6));
+console.log(testJackpot(jackpotTest7));
+console.log(testJackpot(jackpotTest8));
+console.log(testJackpot(jackpotTest9));
+console.log(testJackpot(jackpotTest10));
+
+
 // })();
